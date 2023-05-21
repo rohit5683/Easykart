4d2c9e22425b04b31ca837101ca2b90729412fe9
rohit5683 <killerrohitrocks007@gmail.com>
2023-05-22 01:56:45 +0530

    We performed Category CRUD Operations. Now we can create, read, update and delete category

--- backend/controller/categoryController.js
+++ backend/controller/categoryController.js
@@ -1,5 +1,6 @@
 import categoryModel from "../models/categoryModel.js";
 import slugify from "slugify";
+
 export const createCategoryController = async (req, res) => {
   try {
     const { name } = req.body;
@@ -28,6 +29,92 @@ export const createCategoryController = async (req, res) => {
       success: false,
       error,
       message: "Error in Category",
+    });
+  }
+};
+
+//update category controller
+export const updateCategoryController = async (req, res) => {
+  try {
+    const { name } = req.body;
+    const { id } = req.params;
+    const category = await categoryModel.findByIdAndUpdate(
+      id,
+      {
+        name,
+        slug: slugify(name),
+      },
+      { new: true }
+    );
+    res.status(200).send({
+      success: true,
+      message: "Category Updated Successfully",
+      category,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      success: false,
+      error,
+      message: "Error while updating category",
+    });
+  }
+};
+
+//getAll Categories
+export const categoryController = async (req, res) => {
+  try {
+    const category = await categoryModel.find({});
+    res.status(200).send({
+      success: true,
+      message: "All Categories List",
+      category,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      success: false,
+      error,
+      message: "Error while getting all categories",
+    });
+  }
+};
+
+//single category
+export const singleCategoryController = async (req, res) => {
+  try {
+    const { slug } = req.params;
+    const category = await categoryModel.findOne({ slug: req.params.slug });
+    res.status(200).send({
+      success: true,
+      message: "Get Single Category Successfully",
+      category,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      success: false,
+      error,
+      message: "Error while getting Single Category",
+    });
+  }
+};
+
+//delete category
+export const deleteCategoryController = async (req, res) => {
+  try {
+    const { id } = req.params;
+    await categoryModel.findByIdAndDelete(id);
+    res.status(200).send({
+      success: true,
+      message: "Category Deleted Successfully",
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      success: false,
+      message: "Error while Deleting Category",
+      error,
     });
   }
 };
--- backend/routes/categoryRoute.js
+++ backend/routes/categoryRoute.js
@@ -1,15 +1,44 @@
 import express from "express";
 import { isAdmin, requireSignIn } from "../middleware/authMiddleware.js";
-import { createCategoryController } from "../controller/categoryController.js";
+import {
+  categoryController,
+  createCategoryController,
+  deleteCategoryController,
+  singleCategoryController,
+  updateCategoryController,
+} from "../controller/categoryController.js";
 
 const router = express.Router();
 
 //routes
+//create category
 router.post(
   "/create-category",
   requireSignIn,
   isAdmin,
   createCategoryController
+);
+
+//update category
+router.put(
+  "/update-category/:id",
+  requireSignIn,
+  isAdmin,
+  updateCategoryController
+);
+
+//getAll Category
+router.get("/get-category", categoryController);
+
+//single category
+router.get("/single-category/:slug", singleCategoryController);
+
+//delete category
+router.delete(
+  "/delete-category/:id",
+  requireSignIn,
+  isAdmin,
+  deleteCategoryController
 );
 
 export default router;
