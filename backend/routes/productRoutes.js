9956d063dff5ebd4444921e0b84dcae860791cbf
rohit5683 <killerrohitrocks007@gmail.com>
2023-05-22 15:23:25 +0530

    All CRUD Operations is performed with products and run successfully.

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,6 +1,13 @@
 import express from "express";
 import { isAdmin, requireSignIn } from "../middleware/authMiddleware.js";
-import { createProductController } from "../controller/productController.js";
+import {
+  createProductController,
+  deleteProductController,
+  getProductController,
+  getSingleProductController,
+  productPhotoController,
+  updateProductController,
+} from "../controller/productController.js";
 import formidable from "express-formidable";
 
 const router = express.Router();
@@ -13,5 +20,26 @@ router.post(
   formidable(),
   createProductController
 );
+
+//routes
+router.put(
+  "/update-product/:pid",
+  requireSignIn,
+  isAdmin,
+  formidable(),
+  updateProductController
+);
+
+//get products
+router.get("/get-product", getProductController);
+
+//get single product
+router.get("/get-product/:slug", getSingleProductController);
+
+// get photo
+router.get("/product-photo/:pid", productPhotoController);
+
+//delete product
+router.delete("/product/:pid", deleteProductController);
 
 export default router;
