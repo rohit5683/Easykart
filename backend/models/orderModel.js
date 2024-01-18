f6c2cde90027608dab7dd56f5b70fa9d814b23f0
rohit5683 <killerrohitrocks007@gmail.com>
2024-01-19 01:03:08 +0530

    User Order and Admin order paged is created with functionality and Admin order Status Change Functionality also added

--- backend/models/orderModel.js
+++ backend/models/orderModel.js
@@ -1,22 +1,25 @@
 import mongoose from "mongoose";
 
-const orderSchema = new mongoose.Schema({
-  products: [
-    {
+const orderSchema = new mongoose.Schema(
+  {
+    products: [
+      {
+        type: mongoose.ObjectId,
+        ref: "Products",
+      },
+    ],
+    payment: {},
+    buyer: {
       type: mongoose.ObjectId,
-      ref: "Products",
+      ref: "users",
     },
-  ],
-  payment: {},
-  buyer: {
-    type: mongoose.ObjectId,
-    ref: "Users",
+    status: {
+      type: String,
+      default: "Not Process",
+      enum: ["Not Process", "Processing", "Shipped", "Deliverd", "Cancel"],
+    },
   },
-  status: {
-    type: String,
-    default: "Not Process",
-    enum: ["Not Process", "Processing", "Shipped", "Delivered", "Cancel"],
-  },
-});
+  { timestamps: true }
+);
 
-export default mongoose.model("order", orderSchema);
+export default mongoose.model("Order", orderSchema);
--- backend/routes/authRoute.js
+++ backend/routes/authRoute.js
@@ -5,6 +5,9 @@ import {
   testController,
   forgotPasswordController,
   updateProfileController,
+  getOrdersController,
+  getAllOrdersController,
+  orderStatusController,
 } from "../controller/authController.js";
 import { isAdmin, requireSignIn } from "../middleware/authMiddleware.js";
 
@@ -36,5 +39,19 @@ router.get("/admin-auth", requireSignIn, isAdmin, (req, res) => {
 
 //update profile
 router.put("/profile", requireSignIn, updateProfileController);
+
+//orders
+router.get("/orders", requireSignIn, getOrdersController);
+
+//All orders
+router.get("/all-orders", requireSignIn, isAdmin, getAllOrdersController);
+
+//Order Status update
+router.put(
+  "/order-status/:orderId",
+  requireSignIn,
+  isAdmin,
+  orderStatusController
+);
 
 export default router;
